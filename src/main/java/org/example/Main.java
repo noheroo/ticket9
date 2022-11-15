e7ec9d1ab8408e613bfa7c8b83384ec9bed29ab5
noheroo <serega1552@yandex.ru>
2022-11-16 01:30:04 +0400

    ticket9

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,28 @@
-package org.example;public class Main {
+package org.example;
+
+import org.example.exception.SmthNotFoundException;
+import org.example.repository.FirstRepository;
+
+public class Main {
+    private FirstRepository firstRepository;
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public String doSmth(int id){
+
+        return firstRepository.findById(id).orElseThrow(SmthNotFoundException::new);
+    }
+
+    public void firstMethod(int id){
+        try{
+            String s = doSmth(id);
+        } catch(SmthNotFoundException e){
+            System.out.println("Our Exception here");
+        } catch(RuntimeException e){
+            System.out.println("Other Runtime Exception here");
+        } finally{
+            System.out.println("Operation complete");
+        }
     }
 }
